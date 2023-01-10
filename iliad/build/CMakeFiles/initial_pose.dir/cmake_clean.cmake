fb521bd34aa444161fd2fff77cff2b869e939673
m-borraccino <markborra@gmail.com>
2023-01-10 18:27:37 +0100

    -

---
+++ iliad/build/CMakeFiles/initial_pose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/initial_pose.dir/src/initial_pose.cpp.o"
+  "devel/lib/iliad/initial_pose"
+  "devel/lib/iliad/initial_pose.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/initial_pose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
