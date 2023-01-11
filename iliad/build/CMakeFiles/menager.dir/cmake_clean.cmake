0f1eedb66b26daa960e6b2d1773d3fb0365a6554
m-borraccino <markborra@gmail.com>
2023-01-11 23:25:54 +0100

    -

---
+++ iliad/build/CMakeFiles/menager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/menager.dir/src/menager.cpp.o"
+  "devel/lib/iliad/menager"
+  "devel/lib/iliad/menager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/menager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ iliad/build/CMakeFiles/planar_move.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/planar_move.dir/src/planar_move.cpp.o"
+  "devel/lib/iliad/planar_move"
+  "devel/lib/iliad/planar_move.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/planar_move.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
